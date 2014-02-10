52f609164286dd6697768f065d7c9452a0624683
lucas.martins.s <lucas.martins.s@gmail.com>
2014-02-10 21:09:50 +0000

    Primeiro Commit do meu projeto!

---
+++  ferramenta-auxiliar-para-formalizacao-de-sentencas --username lucas.martins.s@gmail.com/ProjetoFinalLucas/src/naturalLanguageProcessing/DuplaTextoProcessado.java
@@ -0,0 +1,13 @@
+package naturalLanguageProcessing;
+
+public class DuplaTextoProcessado 
+{
+	public String _palavra;
+	public String _tag;
+	
+	public DuplaTextoProcessado( String value, String type )
+	{
+		_palavra = value;
+		_tag = type;
+	}
+}
---
+++  ferramenta-auxiliar-para-formalizacao-de-sentencas --username lucas.martins.s@gmail.com/ProjetoFinalLucas/src/view/MainForm.java
@@ -0,0 +1,67 @@
+package view;
+
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import naturalLanguageProcessing.Axioma;
+import naturalLanguageProcessing.DuplaTextoProcessado;
+import naturalLanguageProcessing.Heuristica;
+import fext.FEXT_Handler;
+
+public class MainForm 
+{
+
+	 public static void main(String[] args)
+	 {
+		 FEXT_Handler handler = FEXT_Handler.getInstance();
+		 
+		 
+		 JFrame  frame = new JFrame("Formalizador de sentenças");
+		 
+		 frame.setSize(800,600);
+		 
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
+		 
+		 JLabel Label = new JLabel("Digite o texto em portugues abaixo:");
+
+		 frame.add(Label);
+		
+		 
+
+		
+		JLabel Label2 = new JLabel("Maria comprou um carro. Maria usa verde.");
+		String answer = handler.EnrichText("Maria comprou um carro. Maria usa verde.");
+		 
+		frame.add(Label2);
+		
+		naturalLanguageProcessing.Heuristica heuristica = new Heuristica(answer);
+		
+		List<Axioma> ax = new ArrayList<Axioma>(heuristica.getAxioms());
+		String resp = "";
+		for ( Axioma axioma:ax )
+		{
+			resp += axioma._subject._palavra; resp += " ";
+			resp += axioma._verb;resp += " ";
+			for ( DuplaTextoProcessado duplaText : axioma._predicates)
+			{
+				resp+=duplaText._palavra;resp += " ";
+			}
+			
+		}
+		
+		JLabel Label3 = new JLabel(resp);
+		frame.add(Label3);
+		
+		 
+		frame.setVisible(true);
+		 
+		frame.setLayout(new FlowLayout());
+		 
+		
+	 }
+}
