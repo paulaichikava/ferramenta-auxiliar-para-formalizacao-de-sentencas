6e83984620bd5d905031f9ae06b32d63a5b65564
lucas.martins.s <lucas.martins.s@gmail.com>
2014-04-17 03:41:24 +0000

    Mudança na ProposicaoTag.
    *Adaptação para utilização de Patterns ( regexp ) ao invés de somente expressões.

---  ferramenta-auxiliar-para-formalizacao-de-sentencas --username lucas.martins.s@gmail.com/ProjetoFinalLucas/src/naturalLanguageProcessing/ProposicaoTag.java
+++  ferramenta-auxiliar-para-formalizacao-de-sentencas --username lucas.martins.s@gmail.com/ProjetoFinalLucas/src/naturalLanguageProcessing/ProposicaoTag.java
@@ -1,19 +1,23 @@
 package naturalLanguageProcessing;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class ProposicaoTag 
 {
 	private String _matchCorpo; // Representa como a expressão é estruturada para a lista de regExp ( _listaRegExpDeProposicoesAtomicas) conseguir capturar as suas proposições atômicas.
-	private List<String> _listaRegExpDeProposicoesAtomicas;
+	private List<Pattern> _listaRegExpDeProposicoesAtomicas;
+	private int _tipoDeExtracao; // Aqui eu defino se poso usar o match para conseguir exatamente o que quero ou se preciso subtrair ele da string "pai". Caso esse int seja 1 subtraio, caso 0 uso o match puro.
 	private String _operadorLogico; // A logic form é a forma lógica dessa ProposicaoTag, ela representa cada proposicao atomica na mesma ordem que ela é descria na listaRegExpDeProposicoesAtomicas.
 	
-	public ProposicaoTag( String id, List<String> lista, String Logic )
+	public ProposicaoTag( String id, List<Pattern> lista, String Logic , int tipo )
 	{
 		_matchCorpo = id;
 		_listaRegExpDeProposicoesAtomicas = lista;
 		_operadorLogico = Logic;
+		_tipoDeExtracao = tipo;
 	}
+	
 	
 	public String getIdRegexp()
 	{
@@ -34,7 +38,7 @@ public class ProposicaoTag
 	 * @param numeroDaProp Número da proposição que vou capturar.
 	 * @return
 	 */
-	public String getRexpForNprop( int numeroDaProp)
+	public Pattern getRexpForNprop( int numeroDaProp)
 	{
 		if ( numeroDaProp <= _listaRegExpDeProposicoesAtomicas.size() )
 			return _listaRegExpDeProposicoesAtomicas.get(numeroDaProp);
@@ -45,6 +49,11 @@ public class ProposicaoTag
 	public String getOperadorLogico()
 	{
 		return _operadorLogico;
+	}
+	
+	public int getTipoDeExtracao()
+	{
+		return _tipoDeExtracao;
 	}
 	
 }
